b7c68675e972f3d2832edc3dbc219a6bbd5de4b2
Marcela <62770219+CoffeeladyCZ@users.noreply.github.com>
2021-05-22 20:48:30 +0200

    refactorig code and save

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import listCoffeehouse from './data.js'
+
 // Variales
 const coffeehouse = document.querySelector('.list-coffeehouse'),
       markers = document.querySelectorAll('#letna, #karlin, #vinohrady, #nusle, #dejvice, #centrum'),
@@ -109,7 +111,7 @@ const listCoffeehouse = [
 {
   name: 'Kontejner',
   address: 'Ortenovo nám. 169, 170 00 Praha 7-Holešovice',
-  district: 'All',
+  district: 'All Holešovice',
   time: 'Po - Pá: 8:00 - 18:00, So - Ne: 10:00 - 18:00',
   content: ' je netradiční kavárna, která na tomto místě chyběla.',
   lat: 50.10782923945228, 
@@ -123,7 +125,34 @@ const listCoffeehouse = [
   content: '',
   lat: 50.076046307224324,
   lng: 14.440395740961378,
-}
+},
+{
+  name: 'Cafefin',
+  address: 'nám. J. z Poděbrad 1407/4, 120 00 Vinohrady',
+  district: 'Vinohrady',
+  time: 'Po - So: 9:00 - 20:00, Ne: 9:00 - 19:00',
+  content: '',
+  lat: 50.078230491278774, 
+  lng: 14.448439963363745
+},
+{
+  name: 'Botanica Coffee Truck',
+  address: 'Moskevská 372/35, 101 00 Praha 10-Vršovice',
+  district: 'Vršovice',
+  time: 'Po - Ne: 9:00 - 20:00',
+  lat: 50.069417672774016, 
+  lng: 14.455258250993772
+},
+{
+  name: '20m2',
+  address: 'Bělohorská 29, 169 00 Praha 6',
+  district: 'Ostatní',
+  time: 'Po - Pá: 7:01 - 19:01, So - 12:01 - 17:01',
+  content: '',
+  lat: 50.08459124214152, 
+  lnt: 14.375977737345796
+},
+
 ];
 
 // Map
@@ -185,10 +214,30 @@ function initMap() {
     infoWindow.open(window.map);
   }
 
+  function animationMarker(wrapper, marker) {
+    document.querySelectorAll('.coffeehouse.coffeehouse__hover').forEach(item => {
+      item.click();
+    })
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+      wrapper.classList.remove('coffeehouse__hover')
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      wrapper.classList.add('coffeehouse__hover');
+    }
+     
+    window.map.addListener('click', () => {
+      marker.setAnimation(null);
+      wrapper.classList.remove('coffeehouse__hover');
+    })
+  }
+
   // Add list
-  let currentMarkers = []
+  let currentMarkers = [];
+  let wrapper;
+
   function addList({name, address, time, lat, lng, content}) {
-    let wrapper = document.createElement('div')
+    wrapper = document.createElement('div')
     wrapper.classList.add('coffeehouse')
     wrapper.innerHTML = `
         <h4 class="list-name">${name}</h4>
@@ -196,33 +245,26 @@ function initMap() {
         <p class="list-time">${time}</p>
     `
     coffeehouse.appendChild(wrapper);
+    
 
-    let marker = new google.maps.Marker({  //vytvoří markery, které patří k vyfilrovaným kavárnám
+    let marker = new google.maps.Marker({  //vytvoří marker
       position: new google.maps.LatLng(lat, lng),
       icon: icons[0].icon,
       animation: google.maps.Animation.DROP,
       title: name,
       map: window.map,
     })
-     
-    // Animation marker
-    marker.addListener('click', animationMarker);
-    wrapper.addEventListener('click', animationMarker);
 
-    function animationMarker() {
-      if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-        wrapper.classList.remove('coffeehouse__hover')
-      } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-        wrapper.classList.add('coffeehouse__hover');
-      }
-       ;
-      window.map.addListener('click', () => {
-        marker.setAnimation(null);
-        wrapper.classList.remove('coffeehouse__hover');
-      })
-    }
+    
+         
+    // Animation marker
+    marker.addListener('click', () => {
+      animationMarker(wrapper, marker)
+    });
+    wrapper.addEventListener('click', () => {
+      animationMarker(wrapper, marker)
+    });
+    
   
     // Create InfoWindow
     const infoWindow = new google.maps.InfoWindow({ 
@@ -234,10 +276,10 @@ function initMap() {
         <div id="bodyContent"><p><b>${name}</b>${content}</div>`,
     });
 
-    // InfoWindow event
-    marker.addListener('click', () => {
+    marker.addListener('click', () => {      
       infoWindow.open(window.map, marker);
     })
+
     // Close InfoWindow
     window.map.addListener('click', () => {
       infoWindow.close(window.map, marker);
@@ -281,18 +323,17 @@ function initMap() {
     })
   }
 
-  // // Loading
-  // function loadingList() {                          DODĚLAT
-  //   let newArray = [];
-  //   listCoffeehouse.forEach(array, () => {
-  //     newArray
-  //   })
-  // }
+  // Carousel
+ 
+
+
+
+
 
 }
 
 
-// nová funkce
+// nová funkce Stránkování
 // vytvořím nové pole, kam se načtou pouze první 4 položky a při každém kliknutí na "odkaz"
 // spustím event, který smaže původní pole a načte nové s dalšími 4 položkami.
 
